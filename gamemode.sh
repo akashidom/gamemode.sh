e9b1eb3e5c20ff94c7331a51fb736af9df7b8333
akashidom <akashidomon@gmail.com>
2026-08-28 01:26:55 +0800

    format gamemode.sh

--- gamemode.sh
+++ gamemode.sh
@@ -5,27 +5,27 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
-cache=$HOME"/.cache/gamemode/"
-state=$cache"/power-profiles-daemon.state"
+cache="$HOME/.cache/gamemode/"
+state="$cache/power-profiles-daemon.state"
 ppd="performance"
 
 case "$1" in
   start)
     echo "Starting gamemode..."
-      mkdir -pv $cache
-      if [ ! -f $state ]; then
-        powerprofilesctl get | tee $state
-      fi
-      powerprofilesctl set $ppd
-      systemctl stop ananicy-cpp.service
+    mkdir -pv "$cache"
+    if [ ! -f "$state" ]; then
+      powerprofilesctl get | tee "$state"
+    fi
+    powerprofilesctl set "$ppd"
+    systemctl stop ananicy-cpp.service
     ;;
   end)
     echo "Ending gamemode..."
-      if [ -f $state ]; then
-        powerprofilesctl set $(cat $state)
-        rm $state
-      fi
-      systemctl start ananicy-cpp.service
+    if [ -f "$state" ]; then
+      powerprofilesctl set "$(cat "$state")"
+      rm "$state"
+    fi
+    systemctl start ananicy-cpp.service
     ;;
   *)
     echo "Invalid argument: $1"
